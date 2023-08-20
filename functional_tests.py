a9a8417003d9827f49c58461308bdaa02c95f81e
Engy-Mohamed <e.mohamed1984eg@gmail.com>
2023-08-20 15:17:17 +0300

    functionl test now checks we can test a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,7 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 
@@ -16,19 +19,31 @@ class MyVisitorTest(unittest.TestCase):
 
         #she notice the page title
         self.assertIn ('To-Do' ,self.browser.title) 
+        header_text = self.browser.find_element(By.TAG_NAME,'h1').text
+        self.assertIn ('To-Do' ,header_text) 
 
         #she invited to enter a to-do Item straight away
-        self.fail("Finish the test")
+        input_box = header_text = self.browser.find_element(By.ID,'id_new_item')
+        self.assertEquals(input_box.get_attribute("placeholder"),"Enter a to-do item")
+        
         #she typed buy rice in text box
+        input_box.send_keys("buy rice")
 
         #when she hits enter the page update and now the item list
         #"1:buy rice"
+        input_box.send_keys(Keys.ENTER)
+        time.sleep(3)
+
+        table = self.browser.find_element(By.ID,"id_list_table")
+        rows = table.find_elements(By.TAG_NAME,"tr")
+        self.assertTrue(any(row.text == "1:buy rice" for row in rows))
+
 
         #there is still a text box inviting it to enter another item
         #she enters buy tomatos to make fatah
 
         #the page updates again and now shows the two items in the list
-
+        self.fail("Finish the test")
         #she goes back to sleep
 
 if __name__ == "__main__":
