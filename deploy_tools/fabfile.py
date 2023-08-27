4f61dc8ec35931a504cdb57ba76a304a068f05ec
Engy-Mohamed <e.mohamed1984eg@gmail.com>
2023-08-27 13:15:31 +0300

    solving issues with fabfile

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -4,20 +4,20 @@ from fabric.api import cd, env, local, run
 
 REPO_URL ="https://github.com/Engy-Mohamed/python-tdd-book.git"
 
-def _get_latest_source():
+def _get_latest_source(site_folder):
     if exists('.git'):
         run('git fetch')
     else:
-        run(f'git clone {REPO_URL}')
+        run(f'git clone {REPO_URL} .')
     # get the id of the current commit
     current_commit = local("git log -n 1 --format=%H",capture=True)
     # as git pull but it is used to override any local changes
-    run(f'git reset --hard {current_commit}')
+    run(f'sudo git reset --hard {current_commit}')
 
 def _update_virtualenv():
     if not exists('virtualenv/bin/pip'):
         run('python3 -m venv virtualenv')
-    run('virtualenv/bin/pip install -r requirements.txt')
+    run('./virtualenv/bin/python ./virtualenv/bin/pip install -r requirements.txt')
 
 def _create_or_update_dotenv():
     # append add it if not exists
@@ -41,10 +41,10 @@ def _update_database():
     run('./virtualenv/bin/python manage.py migrate --noinput')
 
 def deploy():
-    site_folder =f"home/{env.user}/sites/{env.host}"
+    site_folder =f"/home/{env.user}/sites/{env.host}"
     run(f"mkdir -p {site_folder}")
     with cd(site_folder):
-        _get_latest_source()
+        _get_latest_source(site_folder)
         _update_virtualenv()
         _create_or_update_dotenv()
         _update_static_files()
