246113d32de9c4b8cfbbf678655a82af842871cd
Engy-Mohamed <e.mohamed1984eg@gmail.com>
2023-09-14 21:04:10 +0300

    using the new form in item list

--- functional_tests/test_layout_and_styling.py
+++ functional_tests/test_layout_and_styling.py
@@ -15,14 +15,14 @@ class LayoutAndStylingTest(FunctionalTest):
         self.browser.get(self.live_server_url)
 
         # her browser window is set to a specific size
-        self.browser.set_window_size(700, 768)
+        self.browser.set_window_size(600, 768)
 
         # she notices the input box is nicely centered
         input_box = self.get_item_input_box()
 
         self.assertAlmostEqual(
             input_box.location['x'] + input_box.size['width'] / 2,
-            350,
+            300,
             delta=10,
         )
         
--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -2,7 +2,10 @@ from django.test import TestCase
 from lists.views import home_page
 from lists.models import Item, List
 from django.utils.html import escape
-from lists.forms import EMPTY_ITEM_ERROR, ItemForm
+from lists.forms import (
+    DUPLICATE_ITEM_ERROR,EMPTY_ITEM_ERROR,
+    ExistingListItemForm,  ItemForm
+    )
 from unittest import skip
 
 
@@ -94,10 +97,10 @@ class ListViewTest(TestCase):
     def test_displays_item_form(self):
         list_ = List.objects.create()
         response = self.client.get(f'/lists/{list_.id}/')
-        self.assertIsInstance(response.context['form'], ItemForm)  
+        self.assertIsInstance(response.context['form'], ExistingListItemForm)  
         self.assertContains(response, 'name="text"')
 
-    @skip
+
     def test_duplicate_item_validation_errors_end_up_on_lists_page(self):
         list_ = List.objects.create()
         item_ = Item.objects.create(list=list_, text='cook')
@@ -106,11 +109,16 @@ class ListViewTest(TestCase):
             data={'text':'cook'}
         )
 
-        expected_error = escape("you 've already got this in your list")
+        expected_error = escape(DUPLICATE_ITEM_ERROR)
         self.assertContains(response, expected_error)
         self.assertTemplateUsed(response, 'list.html')
         self.assertEqual(Item.objects.all().count(), 1)
 
+    def test_for_invalid_input_passes_form_to_template(self):
+        response = self.post_invalid_input()
+        self.assertIsInstance(response.context['form'], ExistingListItemForm)
+
+        
 class NewListTest(TestCase):
 
     def test_can_save_a_post_request(self):
